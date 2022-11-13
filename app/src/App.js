287aebc5a26d30aa76c299f802fc632027c6c28b
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-13 23:22:45 +0000

    nav works

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Socials from './components/Socials'
 import AboutMe from './components/AboutMe'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -11,6 +13,9 @@ const App = () => {
         <Navbar />
         <Home />
         <AboutMe />
+        <Portfolio />
+        <Contact />
+
         <Socials />
 
 
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { icons } from 'react-icons/lib'
 import profilepic from '../assets/profilepic.jpg'
-import {GoArrowRight} from 'react-icons'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
     className='h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white'>
       
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-        <div className='flex flex-col justify-center h-4'>
+        <div className='flex flex-col justify-center h-full'>
             <h2 className='text-4xl sm:text-7xl font-bold text-white'>
                 I am a Full Stack Developer
             </h2>
@@ -19,13 +20,21 @@ const Home = () => {
                 when an unknown printer took a galley of type and scrambled it to make a type 
             </p>
                 <div>
-                    <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                    <Link to='Portfolio'
+                    smooth
+                    duration={500}
+                    className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                       Portfolio
-                      </button>
+                      <span className='group-hover:rotate-90 duration-300'>
+                      <MdOutlineKeyboardArrowRight size={25} className='m1-1' />
+                      </span>
+                      </Link>
                 </div>
         </div>
         <div>
-            <img src={profilepic} alt='profileimg' className='rounded-2xl mx-auto object-contain h-96 w-96 md:w-full'></img>
+            <img src={profilepic} 
+            alt='profileimg' 
+            className='rounded-2xl mx-auto object-contain h-96 w-96 md:w-full'></img>
         </div>
       </div>
     </div>
