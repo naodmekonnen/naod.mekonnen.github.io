9bc42713b5637b0a1314fcf979405aa2de839616
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-13 00:14:31 +0000

    created feature branch 'Home'

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './index.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div >
+        <Home />
         <Navbar />
     </div>
   )
